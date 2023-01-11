b879088f43b1de082c66baab0cf2a418392f3ff7
Samuel <lee.eunseong7856@gmail.com>
2023-01-11 11:20:10 -0600

    commit tree 944cbcdf9c71df9f689630c93511e3cfcfdfc0c1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    category = %w[Fiction Non-Fiction]
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: category }
+    validate :must_have_words
+
+    def must_have_words
+        # words =~ "Won't Believe Secret Top [number] Guess"
+        unless title.match?("Won't Believe Secret Top [number] Guess")
+            errors.add(:title, "Title must contain key words")
+        end
+    end
 end
